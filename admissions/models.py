8474824bb2ddf969bd6d6b9933c879a4a8ee7efa
kotlamanikanta <kotlamanikanta1998@gmail.com>
2023-03-01 17:56:11 +0530

    second commit

--- admissions/models.py
+++ admissions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=1000)
+    fathername = models.CharField(max_length=1000)
+    classname = models.IntegerField()
+    contact = models.IntegerField(max_length=1000)
+ 
--- admissions/views.py
+++ admissions/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 
 #function based,class based views
+def homepage(request):
+    return render(request,'index.html')
 
 def addAdmission(request):
     values={"name":"mani","age":36,"add":"vzm"}
